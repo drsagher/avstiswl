34865c3992d9ff37aa08670b2300b036e51daae1
unknown <Abubakar Siddique>
2023-09-03 17:50:10 -0700

    v4

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import Hero from "@/app/hero";
 import Service from "@/app/service";
 import About from "@/app/about";
 import Footer from "@/app/footer";
@@ -10,6 +9,7 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover"
+import avsti from "@/public/img/avsti.png";
 
 export default function Home() {
   return (
@@ -55,7 +55,33 @@ export default function Home() {
           </Popover>
           </nav>
       </header>
-      <Hero/>
+
+      {/*<Hero/>*/}
+      <div>
+        <section id='hero-img' className='flex items-center justify-center max-lg:block text-center mt-20 round'>
+          <div className='max-sm:px-6 max-xl:px-20  max-2xl:px-24 max-lg:text-center'>
+            <h1 className='max-2xl:text-xl text-3xl uppercase font-bold leading-normal'>
+              Allied Vocational Skills Training Institute&reg;
+            </h1>
+            <p className='max-2xl:text-sm text-lg font-medium uppercase my-5 text-gray-500'>Centre for empowerment and development of bounded women</p>
+            <Popover>
+              <PopoverTrigger>
+                <div className='bg-blue-500 w-36 h-10 rounded-md text-white uppercase hover:bg-blue-600'>About AVSTI&reg;</div>
+              </PopoverTrigger>
+              <PopoverContent className="flex w-96">
+                <p className="text-sm font-sm">
+                  Allied Vocational Skills Training Institute (AVSTI) is a community development Institute working in Punjab, Pakistan. AVSTI was established in 1990s when a group of volunteers set forth to transform the socio-economic conditions of the population of the country and vicinities in collaboration with community. The institute got registered from Registration Authorities in Pakistan and Sahiwal Chamber of Commerce and Industry in district Sahiwal, Registration No. 3188 to legalize the entire working and to create a better relationship with various international NGOs and GOs. AVSTI is working to reach every person in the community according to their living circumstances and skills trainings of the socio-economic structure. AVSTI is of the view, since the time of its inception, that less income is not the only cause of poverty. In community illiteracy, especially in women, unawareness of human rights, lack of opportunities and adoption of unhealthy old traditions are collectively devastating the social fabric day by day. In such an alarming conditions AVSTI is organizing its efforts to bring a revolutionary change in the seriocomic structure of the areas.
+                </p>
+              </PopoverContent>
+            </Popover>
+          </div>
+
+        </section>
+        <div className='flex justify-center max-sm:px-6 max-xl:px-20  max-2xl:px-24 max-lg:text-center mt-8'>
+          <Image src={avsti} alt='hero' className='max-lg:flex items-center justify-end max-lg:mt-10 max-lg:w-full' width={800} height={600}/>
+        </div>
+      </div>
+      {/*end hero*/}
       <Service/>
       <About/>
       <Footer/>
